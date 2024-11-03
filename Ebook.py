1ab0677c2751404af5a152f12af3c08488bc430f
Shrouq507 <shrouq.g2004@gmail.com>
2024-11-03 17:40:38 +0400

    Update Ebook.py

--- Ebook.py
+++ Ebook.py
@@ -1,1 +1,14 @@
+#Ebook.py
+class Ebook:
+    """Class for an e-book"""
+    def __init__(self, title: str, author: str, pub_date: str, genre: str, price: float):
+        """Initialize an e-book with its details"""
+        self.title = title
+        self.author = author
+        self.pub_date = pub_date
+        self.genre = genre
+        self.price = price
 
+    def __str__(self) -> str:
+        """Return a string that shows the e-book info"""
+        return f"{self.title} by {self.author} - {self.genre} (${self.price:.2f})"
