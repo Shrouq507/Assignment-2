de36bdcf7abcd701337eb85f7857c9c90b14fad4
Shrouq507 <shrouq.g2004@gmail.com>
2024-11-03 18:58:52 +0400

    Update Invoice.py

--- Invoice.py
+++ Invoice.py
@@ -1,1 +1,11 @@
+#Invoice.py
+class Invoice:
+    """Class for creating an invoice for an order"""
+    def __init__(self, order, total_amount):
+        """Create an invoice from an order"""
+        self.order = order
+        self.total_amount = total_amount
 
+    def __str__(self):
+        """Show invoice details"""
+        return "Invoice for Order: " + str(self.order) + ", Total Amount: $" + "{:.2f}".format(self.total_amount)
