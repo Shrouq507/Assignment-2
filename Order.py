0d730af90379405a77e6e60453096f30e99c3a87
Shrouq507 <shrouq.g2004@gmail.com>
2024-11-03 18:56:21 +0400

    Update Order.py

--- Order.py
+++ Order.py
@@ -1,1 +1,13 @@
+#Order.py
+from datetime import datetime
+class Order:
+    """Class for an order made by a customer"""
 
+    def __init__(self, cart):
+        """Initialize an order with a shopping cart"""
+        self.cart = cart  # The shopping cart for this order
+        self.order_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current date and time
+
+    def __str__(self):
+        """Show order details"""
+        return "Order Date: " + self.order_date + ", " + str(self.cart)
