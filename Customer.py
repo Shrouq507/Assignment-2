0781bffce77d06e3bba15a3ec24baaa96662aecd
Shrouq507 <shrouq.g2004@gmail.com>
2024-11-03 18:33:53 +0400

    Update Customer.py

--- Customer.py
+++ Customer.py
@@ -1,1 +1,12 @@
+#Customer.py
+class Customer:
+    """Class for a customer"""
+    def __init__(self, name, contact, is_loyalty_member=False):
+        """Initialize a customer with their info"""
+        self.name = name
+        self.contact = contact
+        self.is_loyalty_member = is_loyalty_member
 
+    def __str__(self):
+        """Return a string that shows the customer info"""
+        return "Customer: " + self.name + ", Contact: " + self.contact + ", Loyalty Member: " + str(self.is_loyalty_member)
