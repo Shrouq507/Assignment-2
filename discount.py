af082981ef331f56c8642d1bdb60d3236a68287a
Shrouq507 <shrouq.g2004@gmail.com>
2024-11-03 19:11:43 +0400

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,1 +1,15 @@
+#Discount.py
+class Discount:
+    """Class for a discount that can be applied to an order"""
+    def __init__(self, description, percentage):
+        """Create a discount"""
+        self.description = description
+        self.percentage = percentage
 
+    def apply(self, total):
+        """Apply the discount to a total"""
+        return total * (1 - self.percentage / 100)
+
+    def __str__(self):
+        """Show discount info"""
+        return self.description + ": " + str(self.percentage) + "%"
